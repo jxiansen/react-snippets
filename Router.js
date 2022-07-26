4b4056e4c883b909e2b24ec82758c4da185cab6d
靖峰 <61311196+jxiansen@users.noreply.github.com>
2022-07-26 17:22:00 +0800

    Update Router.js
    阅读并更新注释

--- Router.js
+++ Router.js
@@ -1,105 +1,89 @@
-/*
-
-Implements React Routing in Plain React, without reliance on React-Router or any other libraries.
-
-To use:
-
-In your top-level React Component (e.g. App or Index.js):
-
-- Import Router (e.g.: import Router from './Router')
-- Create a const with your routes and their associated component
-- Create a const with the component to show on urls not routed (i.e. 404 page)
-- Return a Router component as the top-level component of your App
-
+/* 
+自定义实现 React-router 不使用其他库
+使用方法:
+在react组件的最顶层:
+- 导入Router (e.g.: import Router from './Router')
+- 创建一个数组对象来关联 path 和对应的组件
+- 创建404组件,用来作为没有匹配到的情况
+- 返回一个 Router 组件作为最顶层的 Router 组件
 Example:
-
-```function App() {
-   ...
+```
+function App() {
    const routes = [{path:"/", component:<Home/>}, {path:"/register", component:<Register/>}]
-   ...
    const defaultComponent = <NoPageExists/>
-
+   
    return (
       <Router routes={routes} defaultComponent={defaultComponent}/>
    )
 }
 ```
 
-Then to use routes:
-
-- Use <a href> as you would normally do, e.g. <a href="/register">Register</a>
-
-- If you want to add an onClick event handler to buttons etc. use the `navigate` function, e.g.:
-
+接下来是使用 routes:
+- 使用 <a href> 就像你平时使用的那样,比如. <a href="/register">Register</a>
+- 如果你想要添加一个点击事件处理器给按钮或者其他,使用 `navigate` function,例如:
   <Button onClick={(e) => navigate("/register")} fullWidth>Register</Button>
-
-And that's it!
-
 */
 
+import React from "react";
+import { useEffect, useState } from "react";
 
-/* Code Starts Here */
-
-import React from 'react';
-import { useEffect, useState } from 'react';
-
-// Global Event Listener on "click"
+// 全局监听点击事件
 // Credit Chris Morgan: https://news.ycombinator.com/item?id=31373486
 window.addEventListener("click", function (event) {
-    // Only run this code when an <a> link is clicked
-    const link = event.target.closest("a");
-    // Correctly handle clicks to external sites and
-    // modifier keys
-    if (
-        !event.button &&
-        !event.altKey &&
-        !event.ctrlKey &&
-        !event.metaKey &&
-        !event.shiftKey &&
-        link &&
-        link.href.startsWith(window.location.origin + "/") &&
-        link.target !== "_blank"
-    ) {
-        // prevent full page reload
-        event.preventDefault();
-        // Main routing function
-        navigate(link.href);
-    }
+  // 只有点击事件中是由<a></a>标签触发的才进行
+  const link = event.target.closest("a");
+  // 正确的处理对外部网站的点击和修改判断点击事件中是否包含其他键盘按键
+  if (
+    !event.button &&
+    !event.altKey &&
+    !event.ctrlKey &&
+    !event.metaKey &&
+    !event.shiftKey &&
+    link &&
+    link.href.startsWith(window.location.origin + "/") &&
+    link.target !== "_blank"
+  ) {
+    // 阻止页面重新刷新加载
+    event.preventDefault();
+    // 主路由方法
+    navigate(link.href);
+  }
 });
 
-/* Main Component */
+/* 主要组件 */
 
-export default function Router ({routes, defaultComponent}) {
+export default function Router({ routes, defaultComponent }) {
+  // 监听 url 的状态变化,并强制组件在变化的时候重新渲染
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
-    // state to track URL and force component to re-render on change
-    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+  useEffect(() => {
+    // 将回调定义为单独的函数,方便以后可以用清理函数将器删除
+    const onLocationChange = () => {
+      // 更新当前 window 中 URL
+      setCurrentPath(window.location.pathname);
+    };
 
-    useEffect(() => {
-        // define callback as separate function so it can be removed later with cleanup function
-        const onLocationChange = () => {
-            // update path state to current window URL
-            setCurrentPath(window.location.pathname);
-        }
+    // 监听 popstate 事件
+    window.addEventListener("popstate", onLocationChange);
 
-        // listen for popstate event
-        window.addEventListener('popstate', onLocationChange);
-
-        // clean up event listener
-        return () => {
-            window.removeEventListener('popstate', onLocationChange)
-        };
-    }, [])
-    return routes.find(({path, component}) => path === currentPath)?.component || defaultComponent
+    // 移除事件监听器
+    return () => {
+      window.removeEventListener("popstate", onLocationChange);
+    };
+  }, []);
+  return (
+    routes.find(({ path, component }) => path === currentPath)?.component ||
+    defaultComponent
+  );
 }
 
-/* Use the below in buttons and programmatically to navigate to pages */
+/* 使用下面的 navigate 函数来导航到指定页面 */
 
-export function navigate (href) {
+export function navigate(href) {
+  // 更新 url
+  window.history.pushState({}, "", href);
 
-    // update url
-    window.history.pushState({}, "", href);
-
-    // communicate to Routes that URL has changed
-    const navEvent = new PopStateEvent('popstate');
-    window.dispatchEvent(navEvent);
+  // 向 Routes 通知, URL 已经改变
+  const navEvent = new PopStateEvent("popstate");
+  window.dispatchEvent(navEvent);
 }
